bef7b5824e501f08301ee1a95e2179a13c9fa9ef
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2019-01-17 13:46:56 -0500

    Add project metadata

--- mix.exs
+++ mix.exs
@@ -5,9 +5,13 @@ defmodule Crdt.MixProject do
     [
       app: :crdt,
       version: "0.1.0",
+      description: "A library of Conflict-Free Replicated Data Types (CRDTs).",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      package: package(),
+      name: "crdt",
+      source_url: "https://gitlab.com/jeffrey-xiao/crdt_ex",
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         coveralls: :test,
@@ -30,6 +34,16 @@ defmodule Crdt.MixProject do
       {:dialyxir, "~> 1.0.0-rc.4", only: :dev, runtime: false},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:excoveralls, "~> 0.10", only: :test}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT", "Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/jeffrey-xiao/crdt_ex",
+        "GitLab" => "https://gitlab.com/jeffrey-xiao/crdt_ex"
+      }
     ]
   end
 end
