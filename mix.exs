900514fdf075a15d055ce8ca0defd7ba90c9ee18
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2019-06-02 15:19:10 -0400

    format

--- mix.exs
+++ mix.exs
@@ -7,15 +7,18 @@ defmodule Crdt.MixProject do
   def project do
     [
       app: :crdt,
-      name: "crdt",
       version: "0.1.0",
       description: "A library of Conflict-Free Replicated Data Types (CRDTs).",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
+      name: "crdt",
       source_url: @gitlab_url,
       homepage_url: @gitlab_url,
+      docs: [
+        extras: ["README.md"]
+      ],
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         coveralls: :test,
@@ -23,25 +26,28 @@ defmodule Crdt.MixProject do
         "coveralls.post": :test,
         "coveralls.html": :test,
         "coveralls.json": :test
+      ],
+      dialyzer: [
+        plt_add_deps: :transitive
       ]
     ]
   end
 
-  def application do
+  def application() do
     [
       extra_applications: [:logger]
     ]
   end
 
-  defp deps do
+  defp deps() do
     [
-      {:dialyxir, "~> 1.0.0-rc.4", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.6", only: :dev, runtime: false},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 
-  defp package do
+  defp package() do
     [
       files: ["lib", "LICENSE-APACHE", "LICENSE-MIT", "mix.exs", "README.md"],
       licenses: ["Apache 2.0", "MIT"],
